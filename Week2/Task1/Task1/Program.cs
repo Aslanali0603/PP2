c9cfc05e5425687a9fe7acf4ba61813aa6ac228c
Aslanali0603 <aslan.utebay@mail.ru>
2019-02-14 15:13:50 +0600

    New Task

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Аслан\Desktop\PP2/FirstTimeGirl.txt");
+            string s = sr.ReadToEnd();
+            bool t = true;
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] != s[s.Length - 1 - i])
+                    t = false;
+            if (t)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
