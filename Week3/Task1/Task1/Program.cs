ed9ebc9bbdbca61a1f428a301f1ca33cafcb21f0
Aslanali0603 <aslan.utebay@mail.ru>
2019-02-15 19:14:52 +0600

    Added Comments

---
+++ Week3/Task1/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Triangle
+{
+    int a;
+    int b;
+    int c;
+
+    Triangle() { }
+    public Triangle(int a, int b, int c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int Perimetr()
+    { 
+        int p = a + b + c;
+        return p;
+    }
+    public double Square()
+    {
+        double p = ((a + b + c)/2);
+
+        return Math.Sqrt(p * (p - a) * (p - b)*(p - c));
+    }
+    
+}
+namespace Task1
+{
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        tr1 = new tr1;
+        
+        }
+    }
