076bde52dea22e5dfbb88c69ff570fa8ae81686f
Aslanali0603 <aslan.utebay@mail.ru>
2019-02-15 15:37:27 +0600

    added comments

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)                             // Method to print spaces
+        {
+            for (int i = 0; i < level; i++)                            // starts from 0 ends at level
+                Console.Write(" ");                                    
+        }
+
+        static void GetInfo(DirectoryInfo directory, int level)        // Method with 2 parametres
+        {
+            FileInfo[] files = directory.GetFiles();                   // array files
+            DirectoryInfo[] directories = directory.GetDirectories();  // array directories
+
+            foreach(FileInfo file in files)                            // Check arr files
+            {
+                PrintSpaces(level);                                    // calls method PrintSpaces
+                Console.WriteLine(file.Name);                          // outputs name of file
+            }
+
+            foreach(DirectoryInfo d in directories)                    // Check arr directories
+            {
+                PrintSpaces(level);                                    // Calls method PrintSpaces
+                Console.WriteLine(d.Name);                             // Outputs name of directory
+                GetInfo(d, level + 1);                                 // Calls method GetInfo with level++
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Аслан\Desktop\PP2\PP2\Week2"); // Gets directory info
+            GetInfo(d, 0);                                                                // calls method GetInfo with level 0
+
+        }
+    }
+}
---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Creation(string newpath)                         // Method to create
+        {
+            Directory.CreateDirectory(newpath);                      // Create new directory
+            string filename = "school.txt";                          // our new file will be shcool.txt for exapmle
+            newpath = Path.Combine(newpath, filename);               // combine direction and name 
+            FileStream fs = File.Create(newpath);                    // create file and create filestream
+            fs.Close();                                              // close and save filesream
+            StreamWriter sw = new StreamWriter(newpath);             // create writer
+            sw.Write("Mysyk is very pysyk!");                        // writing in txt
+            sw.Close();                                              // save and close
+            Copying(newpath);                                        // call method Copying
+            Deleting(newpath);                                       // call method Deleting
+        }
+        public static void Copying(string newpath)
+        {
+            string target = @"C:\Users\Аслан\Desktop\PP2/HelloMyFriend!.txt";  // target path
+            File.Copy(newpath, target);                                        // copying from source newpath to target path
+        }
+        public static void Deleting(string newpath)
+        {
+            File.Delete(newpath);                                              // deleting first file
+        }
+        static void Main(string[] args)
+        {
+            string newpath = @"C:\Users\Аслан\Desktop\PP2/local";            //Creating string which consists path
+            Creation(newpath);                                               //call method to create
+        }
+
+     
+        
+    }
+}
